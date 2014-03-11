5b6a115fad24ff19441334a59c9b6ff82ef7deb7
kazsharp <kaz3391@gmail.com>
2014-03-11 11:16:30 +0900

    javadoc用package-info.javaを追加

---
+++ src/main/java/jp/gr/java_conf/kazsharp/pretendbdd/package-info.java
@@ -0,0 +1,14 @@
+/*
+ BddRunner.java
+
+ Copyright (c) 2014 KazushigeSasaki
+
+ This software is released under the MIT License.
+
+ http://opensource.org/licenses/mit-license.php
+
+ */
+/**
+ * PretendBDDのクラス群.
+ */
+package jp.gr.java_conf.kazsharp.pretendbdd;
